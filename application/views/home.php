366f0b80e72fb786fa2fe87ae9ff1a5b663c3725
Patrick Cockwell <pcockwell@gmail.com>
2012-09-15 04:31:39 -0400

    styling

--- application/views/home.php
+++ application/views/home.php
@@ -4,12 +4,79 @@ $this->load->view('header');
 
 ?>
 <div class="container">
-    <div class="row">
+    <div class="row-fluid">
     	<div class="span8 schedule">
     		<h4>Today's Schedule</h4>
-    		<p>Hello World</p>
-    		<p>Hello World</p>
-    		<p>Hello World</p>
+    		<div class="row-fluid">
+    			<div class="span1 priority-static">
+    			</div>
+    			<div class="span3">
+    				12AM-10AM
+    			</div>
+    			<div class="span8">
+    				SLEEP
+    			</div>
+    		</div>
+    		<div class="row-fluid">
+    			<div class="span1 priority-static">
+    			</div>
+    			<div class="span3">
+    				11AM-12:30PM
+    			</div>
+    			<div class="span8">
+    				CLASS
+    			</div>
+    		</div>
+    		<div class="row-fluid">
+    			<div class="span1 priority-high">
+    			</div>
+    			<div class="span3">
+    				12:30PM-2:30PM
+    			</div>
+    			<div class="span8">
+    				CS ASSIGNMENT
+    			</div>
+    		</div>
+    		<div class="row-fluid">
+    			<div class="span1 priority-static">
+    			</div>
+    			<div class="span3">
+    				2:30PM-5PM
+    			</div>
+    			<div class="span8">
+    				WORKOUT
+    			</div>
+    		</div>
+    		<div class="row-fluid">
+    			<div class="span1 priority-high">
+    			</div>
+    			<div class="span3">
+    				5PM-8PM
+    			</div>
+    			<div class="span8">
+    				CS ASSIGNMENT
+    			</div>
+    		</div>
+    		<div class="row-fluid">
+    			<div class="span1 priority-low">
+    			</div>
+    			<div class="span3">
+    				8PM-9PM
+    			</div>
+    			<div class="span8">
+    				STUDY ANTHRO
+    			</div>
+    		</div>
+    		<div class="row-fluid">
+    			<div class="span1 priority-static">
+    			</div>
+    			<div class="span3">
+    				11PM-12AM
+    			</div>
+    			<div class="span8">
+    				SLEEP
+    			</div>
+    		</div>
     	</div>
     	<div class="span4 info-bar">
     		<h4>Information</h4>
