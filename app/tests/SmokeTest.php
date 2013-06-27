5c4fe5142cd07b8d986c8a80bc9756fc990aa068
fydp <fydp@fydp-VirtualBox.(none)>
2013-06-27 14:34:26 -0400

    Ignoring swp files; phpunit is pulled in composer dev install; added first smoke test for #16

---
+++ app/tests/SmokeTest.php
@@ -0,0 +1,21 @@
+<?php
+
+class SmokeTest extends TestCase {
+
+    /**
+     * Test that the server is up.
+     *
+     * Hits the 'hello' page.
+     * Should produce "Hello `name`"
+     *
+     * @return void
+     */
+    public function testServerUp()
+    {
+        $test_name = 'TEST';
+        $response = $this->call('GET', 'api/hello/'.$test_name);
+        $this->assertEquals('Hello '.$test_name, 
+            $response->getContent());
+    }
+
+}
