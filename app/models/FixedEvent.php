1d0f2c0e86127475aedab33f19a31bd77a29dd08
Patrick Cockwell <pcockwell@gmail.com>
2013-07-05 10:07:59 -0400

    FixedEvent model fillables

--- app/models/FixedEvent.php
+++ app/models/FixedEvent.php
@@ -9,4 +9,9 @@ class FixedEvent extends Eloquent {
 	 */
 	protected $table = 'fixed_events';
 
+	protected $fillable = array('user_id', 'name', 'start_time', 'end_time', 'start_date', 'end_date', 'recurrences');
+
+	public function getDates(){
+		return array('created_at', 'updated_at', 'start_date', 'end_date');
+	}
 }
