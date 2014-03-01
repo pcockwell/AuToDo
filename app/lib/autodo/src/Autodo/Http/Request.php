32044cc48b353d241dbe6d1fe01cc0e173c4b4a8
Patrick Cockwell <pcockwell@gmail.com>
2014-03-01 16:54:51 -0500

    xml input works

--- app/lib/autodo/src/Autodo/Http/Request.php
+++ app/lib/autodo/src/Autodo/Http/Request.php
@@ -3,9 +3,43 @@
 namespace Autodo\Http;
 
 use Illuminate\Http\Request as BaseRequest;
+use Symfony\Component\HttpFoundation\ParameterBag;
 
 class Request extends BaseRequest
 {
+
+    /**
+     * Get the XML payload for the request.
+     *
+     * @param  string  $key
+     * @param  mixed   $default
+     * @return mixed
+     */
+    public function xml($key = null, $default = null)
+    {
+        if ( ! isset($this->xml))
+        {
+            $xml = new \SimpleXMLElement($this->getContent(), true);
+            $this->xml = new ParameterBag(json_decode(json_encode($xml), TRUE));
+        }
+
+        if (is_null($key)) return $this->xml;
+
+        return array_get($this->xml->all(), $key, $default);
+    }
+
+    /**
+     * Get the input source for the request.
+     *
+     * @return \Symfony\Component\HttpFoundation\ParameterBag
+     */
+    protected function getInputSource()
+    {
+        if ($this->isJson()) return $this->json();
+        if ($this->isXml()) return $this->xml();
+
+        return $this->getMethod() == 'GET' ? $this->query : $this->request;
+    }
 
     /**
      * Determine if the request is sending JSON.
